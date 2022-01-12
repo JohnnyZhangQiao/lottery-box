fd169c8300166ca44c7302955e49fd39ef028ef3
Johnny <zqiaozhang@tencent.com>
2022-01-12 15:55:40 +0800

    logger

--- src/utils/workflow.ts
+++ src/utils/workflow.ts
@@ -15,16 +15,20 @@ export const initialize = async (app: any) => {
   // 初始化rem
   initRem(window, document.documentElement);
   window.calcRem(1080);
+  console.trace('rem初始化完成...');
 
   // 按需加载nutui组件
   Object.values(nutUiList).forEach(co => {
     app.use(co);
   });
+  console.trace('nutui组件加载完成...');
 
   // 挂载路由
   app.use(router);
+  console.trace('router已挂载...');
 
   // 注册pinia状态管理库
   app.use(createPinia());
   registerStore();
+  console.trace('pinia状态库已注册...');
 };
